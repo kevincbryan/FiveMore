d84d394fad493efd2c9363c62f036cd16b696a0d
Kevin C Bryan <kevincbryan@gmail.com>
2019-12-16 14:27:09 -0800

    Controller Updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 10f;
-    Rigidbody2D rb;
+    public bool isMoving = false;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Debug.Log("PlayerController is running");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+
+        if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            isMoving = true;
+            Debug.Log("getting Input " + Input.GetAxis ("Horizontal") + Input.GetAxis ("Vertical"));
+            Move();
+        }
+        else
+        {
+            Debug.Log("No input");
+            isMoving = false;
+        }
     }
 
     private void Move()
     {
-        rb.velocity = transform.forward * Input.GetAxis("Vertical") * playerSpeed;
+        
+        float moveRight = 0;
+        float moveForward = 0;
+
+        moveForward += Input.GetAxis("Vertical");
+        moveRight += Input.GetAxis("Horizontal");
+
+        Vector3 moveVector  = ((transform.forward * moveForward) + (transform.right * moveRight));
+
+        moveVector.Normalize();
+        moveVector *= playerSpeed * Time.deltaTime;
+        transform.position += moveVector;
     }
 }
