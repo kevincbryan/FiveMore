d2ddd41615eda81077f8024ccc83ccec9ab54c7a
Kevin C Bryan <kevincbryan@gmail.com>
2019-12-16 14:06:11 -0800

    Starting on Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 10f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = transform.forward * Input.GetAxis("Vertical") * playerSpeed;
+    }
+}
