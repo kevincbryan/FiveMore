19c86ad58415fc0d3f0a0b012984263ab3e4e459
Kevin C Bryan <kevincbryan@gmail.com>
2019-12-16 11:52:21 -0800

    Room Generator Work

---
+++ Assets/Scripts/PlaceRooms.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceRooms : MonoBehaviour
+{
+
+    public GameObject[] rooms;
+    public GameObject[] roomLocs;
+    //private GameObject[] roomsPicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShuffleArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ShuffleArray()
+    {
+        int n = rooms.Length;
+
+        while ( n > 1)
+        {
+            n--;
+            int k = Random.Range(1, 0 + 1);
+
+
+        }
+
+    }
+}
