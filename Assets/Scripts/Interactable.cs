a73f2ef189b6e1692687e909b50bd60c17ba2098
Kevin C Bryan <kevincbryan@gmail.com>
2019-12-16 15:30:35 -0800

    Added Interactable Prefab and Script

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+    
+{
+    public bool isUsed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isUsed == false)
+        {
+            if (collision.tag == "Player")
+            {
+                isUsed = true;
+            }
+        }
+    }
+}
